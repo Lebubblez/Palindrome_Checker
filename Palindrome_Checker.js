a7561025928b52ce70244a0e588b9a4717616b5c
Leda Erlandson <leda.erlandson@gmail.com>
2018-09-11 13:43:29 -0600

    Finish
    Added in break at end of else statment, because the for loop would continue looping even when there was a false, so there needed to be a break to end the loop when a false occured.

--- Palindrome_Checker.js
+++ Palindrome_Checker.js
@@ -8,14 +8,15 @@ function palindrome(str) {
     tempRev.unshift(splitStr[c]);
 
   }
+  console.log(splitStr);
   console.log(tempRev);
-
   for (var c in splitStr) {
     if (splitStr[c] === tempRev[c]) {
       rev = true;
     }
     else {
       rev = false;
+      break;
     }
   }
   return rev;
